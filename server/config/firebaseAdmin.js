c0125e2b32653ac8018ec51ed69db81d6cd75e09
ms-rawat <moharsinghzxz@gmail.com>
2025-11-22 11:39:47 +0530

    feat: update firebaseAdmin to use environment variable for service account key

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,6 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config();
 
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, "base64").toString()
+);
+
+console.log(serviceAccount);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
